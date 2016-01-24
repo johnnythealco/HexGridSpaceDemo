5319f0da11377d3e9f2fe0382a78640372eff500
Johnny Kiely <johnnykiely@gmail.com>
2016-01-24 19:34:21 +0000

    Start

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Gamelogic.Grids;
+using Gamelogic;
+
+public class Cell :  SpriteCell
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
